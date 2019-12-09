2d8afd19a0a7eeb99b3ed8d38e912c6daef5ccd5
Charlie Parsons <cdaltonparsons@gmail.com>
2019-12-09 09:42:05 -0700

    added morgan to help debug/troubleshoot

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const morgan = require('morgan')
 
 require('dotenv').config();
 
@@ -10,10 +10,11 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"))
 
 const uri = process.env.MONGODB_URI;
 mongoose.Promise = global.Promise;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}).then(() => console.log("DB server connect")).catch(e => console.log("DB error", e));
+mongoose.connect(uri || process.env.ATLAS_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}).then(() => console.log("DB server connect")).catch(e => console.log("DB error", e));
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully")
